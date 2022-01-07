c8b3e921ecb9271ba19e8e95800db461be0539fb
firechoi <dev@plating.co.kr>
2022-01-07 17:58:52 +0900

    mod me

--- src/screens/profile/me.js
+++ src/screens/profile/me.js
@@ -1,6 +1,16 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import {withTranslation} from 'react-i18next';
+import React from 'react';
+import {createStructuredSelector} from 'reselect';
+import {authSelector} from 'src/modules/auth/selectors';
+import {
+  wishListSelector,
+  configsSelector,
+  languageSelector,
+} from 'src/modules/common/selectors';
+// import {connect} from 'react-redux';
+import {useTranslation} from 'react-i18next';
+import {useNavigation} from '@react-navigation/native';
+
+import {useSelector} from 'react-redux';
 import {StyleSheet, ScrollView, View, Linking} from 'react-native';
 import {Header, ThemedView, Text} from 'src/components';
 
@@ -11,18 +21,29 @@ import Container from 'src/containers/Container';
 import SocialIcon from 'src/containers/SocialIcon';
 import {TextHeader, CartIcon} from 'src/containers/HeaderComponent';
 
-import {authSelector} from 'src/modules/auth/selectors';
-import {
-  wishListSelector,
-  configsSelector,
-  languageSelector,
-} from 'src/modules/common/selectors';
-
 import {grey5} from 'src/components/config/colors';
 import {margin} from 'src/components/config/spacing';
 
-class MeScreen extends Component {
-  icon = name => {
+export default function MeScreen() {
+  const navigation = useNavigation();
+  const {t} = useTranslation();
+  // const dispatch = useDispatch();
+  const stateSelector = createStructuredSelector({
+    auth: authSelector(),
+    wishList: wishListSelector(),
+    configs: configsSelector(),
+    language: languageSelector(),
+  });
+
+  const {
+    auth,
+    wishList,
+    configs,
+    language,
+  } = useSelector(stateSelector);
+
+  const {isLogin} = auth;
+  const icon = name => {
     return {
       name: name,
       size: 18,
@@ -30,85 +51,76 @@ class MeScreen extends Component {
     };
   };
 
-  handleLinkUrl = url => {
+  const handleLinkUrl = url => {
     Linking.openURL(url);
   };
 
-  goPageOther = router => {
-    this.props.navigation.navigate(router);
+  const goPageOther = router => {
+    navigation.navigate(router);
   };
 
-  render() {
-    const {
-      t,
-      configs,
-      auth: {isLogin},
-      language,
-    } = this.props;
-
-    return (
-      <ThemedView isFullView>
-        <Header
-          centerComponent={<TextHeader title={t('common:text_me_screen')} />}
-          rightComponent={<CartIcon />}
-        />
-        <ScrollView>
-          <Container style={styles.viewContent}>
-            <HeaderMe />
-            <InformationMe isLogin={isLogin} clickPage={this.goPageOther} />
-            <SettingMe
-              isLogin={isLogin}
-              clickPage={this.goPageOther}
-              goPhone={this.handleLinkUrl}
-              phonenumber={configs.get('phone')}
+  return (
+    <ThemedView isFullView>
+      <Header
+        centerComponent={<TextHeader title={t('common:text_me_screen')} />}
+        rightComponent={<CartIcon />}
+      />
+      <ScrollView>
+        <Container style={styles.viewContent}>
+          <HeaderMe />
+          <InformationMe isLogin={isLogin} clickPage={goPageOther} />
+          <SettingMe
+            isLogin={isLogin}
+            clickPage={goPageOther}
+            goPhone={handleLinkUrl}
+            phonenumber={configs.get('phone')}
+          />
+          <View style={styles.viewSocial}>
+            <SocialIcon
+              light
+              raised={false}
+              type="facebook"
+              style={styles.socialIconStyle}
+              iconSize={15}
+              onPress={() => handleLinkUrl(configs.get('facebook'))}
             />
-            <View style={styles.viewSocial}>
-              <SocialIcon
-                light
-                raised={false}
-                type="facebook"
-                style={styles.socialIconStyle}
-                iconSize={15}
-                onPress={() => this.handleLinkUrl(configs.get('facebook'))}
-              />
 
-              <SocialIcon
-                light
-                raised={false}
-                type="instagram"
-                style={styles.socialIconStyle}
-                iconSize={15}
-                onPress={() => this.handleLinkUrl(configs.get('instagram'))}
-              />
+            <SocialIcon
+              light
+              raised={false}
+              type="instagram"
+              style={styles.socialIconStyle}
+              iconSize={15}
+              onPress={() => handleLinkUrl(configs.get('instagram'))}
+            />
 
-              <SocialIcon
-                light
-                raised={false}
-                type="pinterest"
-                style={styles.socialIconStyle}
-                iconSize={15}
-                onPress={() => this.handleLinkUrl(configs.get('pinterest'))}
-              />
+            <SocialIcon
+              light
+              raised={false}
+              type="pinterest"
+              style={styles.socialIconStyle}
+              iconSize={15}
+              onPress={() => handleLinkUrl(configs.get('pinterest'))}
+            />
 
-              <SocialIcon
-                light
-                raised={false}
-                type="twitter"
-                style={styles.socialIconStyle}
-                iconSize={15}
-                onPress={() => this.handleLinkUrl(configs.get('twitter'))}
-              />
-            </View>
-            <Text h6 colorThird>
-              {typeof configs.get('copyright') === 'string'
-                ? configs.get('copyright')
-                : configs.getIn(['copyright', language])}
-            </Text>
-          </Container>
-        </ScrollView>
-      </ThemedView>
-    );
-  }
+            <SocialIcon
+              light
+              raised={false}
+              type="twitter"
+              style={styles.socialIconStyle}
+              iconSize={15}
+              onPress={() => handleLinkUrl(configs.get('twitter'))}
+            />
+          </View>
+          <Text h6 colorThird>
+            {typeof configs.get('copyright') === 'string'
+              ? configs.get('copyright')
+              : configs.getIn(['copyright', language])}
+          </Text>
+        </Container>
+      </ScrollView>
+    </ThemedView>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -131,13 +143,5 @@ const styles = StyleSheet.create({
   },
 });
 
-const mapStateToProps = state => {
-  return {
-    auth: authSelector(state),
-    wishList: wishListSelector(state),
-    configs: configsSelector(state),
-    language: languageSelector(state),
-  };
-};
 
-export default connect(mapStateToProps)(withTranslation()(MeScreen));
+// export default connect(mapStateToProps)(withTranslation()(MeScreen));
